ac3046adcf058c9b54006eb0d28e7f9291e04eaf
Emmerich Moreno <boogerprime@gmail.com>
2026-06-13 13:34:29 -0700

    Configured Lab D Frontend for Emmerich

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/team-3/lab-d-Solomon">
+    <BrowserRouter basename="/team-3-s26/lab-d-emmerich">
       <App />
     </BrowserRouter>
   </StrictMode>,
